8b795c97401bb30b8a514e9ad26e03033da1f02a
Jaishree2310 <jaishrees23102001@gmail.com>
2023-07-07 17:21:38 +0530

    My portfolio

---
+++ src/components/Projects.js
@@ -0,0 +1,90 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
+
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Automatic PDF maker",
+      description: "Helps to take down notes",
+      imgUrl: projImg1,
+    },
+    {
+      title: "GPT3",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Healthcare",
+      description: "Helps to diagnose the syptoms",
+      imgUrl: projImg3,
+    },
+    {
+      title: "AI image generative",
+      description: "Develop the text to image",
+      imgUrl: projImg4,
+    },
+    {
+      title: "AI Voice Assistance",
+      description: "text to speech recognition",
+      imgUrl: projImg5,
+      
+    },
+    {
+      title: "Ecommerce website",
+      description: "Things you need to buy",
+      imgUrl: projImg6,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p> I have made several projects including Python such as AI voice assistance, calorie calculator, resume maker, python interpreter, etc.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                  
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
